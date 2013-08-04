470ced22a1620d31141b66b3ff6d71f0b39837c9
Geoff Gifford <geoff@perfectworkflows.co.uk>
2013-08-04 22:00:48 +0100

    added .docx download

--- app/models/apprenticeship_type.rb
+++ app/models/apprenticeship_type.rb
@@ -13,49 +13,70 @@ class ApprenticeshipType < ActiveRecord::Base
 	  where("created_at > ?", time)
 	end
 
-# def render_to_xls(cars, name = 'myname')
 
-# 		book = Spreadsheet::Workbook.new
-# 		data = book.create_worksheet :name => name
-# 		data.row(0).concat %w{car_type miles 2nd_key}
-# 		header_format = Spreadsheet::Format.new :color => :green, :weight => :bold
-# 		data.row(0).default_format = header_format
+def render_to_word(name = 'doc_from_template')
+  file = Rails.root.join("public","resources", "test.docx");
+  inputDir = ::Rails.root.join('app','models','word','apprenticeship_types','template')
+  outputFile = ::Rails.root.join('public','docx',"apprenticeship_type_#{id}.docx")
+  file_to_string  
+  write inputDir, outputFile
+  outputFile
+end
 
-# 		cars.each_with_index { |n, i|
+def file_to_string 
+  templateFile              = ::Rails.root.join('app','models','word', 'apprenticeship_types', 'template_document.xml')
+  file                      = File.open templateFile
+  contents                  = file.read
+  file.close
 
-# 			data.row(i+1).push n.car.car_type.full_name,n.email
-# 		}  
-# 		blob = StringIO.new('')
-# 		book.write(blob)
-# 		blob
-# 	end
-	def render_to_word(name = 'doc_from_template')
+  this_print_title          = ActionController::Base.helpers.sanitize(self.print_title)            unless self.print_title.nil? 
+  this_print_description    = ActionController::Base.helpers.strip_tags(self.print_description)    unless self.print_description.nil? 
+  this_print_qualification  = ActionController::Base.helpers.strip_tags(self.print_qualification)  unless self.print_qualification.nil? 
+  this_print_salary         = ActionController::Base.helpers.strip_tags(self.print_salary)         unless self.print_salary.nil? 
+  this_print_job_role       = ActionController::Base.helpers.strip_tags(self.print_job_role)       unless self.print_job_role.nil? 
+  this_issuing_authority    = ActionController::Base.helpers.strip_tags(self.issuing_authority)    unless self.issuing_authority.nil? 
+  
+contents.gsub!( /::print_title:(.*?):print_title::/ )                         { "#{this_print_title}"             }
+contents.gsub!( /::print_description:(.*?):print_description::/ )             { "#{this_print_description}"       }
+contents.gsub!( /::print_qualification:(.*?):print_qualification::/ )         { "#{this_print_qualification }"    }
+contents.gsub!( /::print_salary:(.*?):print_salary::/ )                       { "#{this_print_salary}"            }
+contents.gsub!( /::print_job_role:(.*?):print_job_role::/ )                   { "#{this_print_job_role}"          } 
+contents.gsub!( /::issuing_authority:(.*?):issuing_authority::/ ) { "#{this_issuing_authority}" }
 
-		# blob =	File.open("path/to/file", "w") { |file|  }
+aFile_path = ::Rails.root.join('app','models','word','apprenticeship_types','template','word', 'document.xml')
+aFile = File.new(aFile_path, "w")
+aFile.write(contents)
+aFile.close
+  
+end
 
- file = Rails.root.join("public","resources", "test.docx");
- file
+def write inputDir, outputFile
 
+  require 'zip/zip'
 
+    FileUtils.rm outputFile,:force => true
+    entries = Dir.entries(inputDir); entries.delete("."); entries.delete("..") 
+    io = Zip::ZipFile.open(outputFile, Zip::ZipFile::CREATE);
+    writeEntries(entries, "", io, inputDir, outputFile)
+    io.close();
+  end
 
-	end
-
-  def create_docx()
-
-    write_path = ::Rails.root.join('public','docx',"apprenticeship_type_#{name}.docx")
-
-     bundle_filename = "abc.zip"
-      FileUtils.rm "abc.zip",:force => true
-      dir = "testruby"
-      Zip::ZipFile.open(bundle_filename, Zip::ZipFile::CREATE) { |zipfile|
-        Dir.foreach(dir) do |item|
-          item_path = "#{dir}/#{item}"
-          zipfile.add( item,item_path) if File.file?item_path
-        end
-      }
-     File.chmod(0644,bundle_filename)
-
-
+  # A helper method to make the recursion work.
+  private
+  def writeEntries(entries, path, io, inputDir, outputFile)
+    
+    entries.each { |e|
+      zipFilePath = path == "" ? e : File.join(path, e)
+      diskFilePath = File.join(inputDir, zipFilePath)
+      puts "Deflating " + diskFilePath
+      if  File.directory?(diskFilePath)
+        io.mkdir(zipFilePath)
+        subdir =Dir.entries(diskFilePath); subdir.delete("."); subdir.delete("..") 
+        writeEntries(subdir, zipFilePath, io, inputDir, outputFile)
+      else
+        io.get_output_stream(zipFilePath) { |f| f.puts(File.open(diskFilePath, "rb").read())}
+      end
+    }
   end
 
 end
