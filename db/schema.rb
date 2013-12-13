e28da828ec47c506acc5daed3e5c6b04171f2281
Gurdipe Dosanjh <rhomees@gmail.com>
2013-12-13 15:37:15 +0000

    merging

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130224213231) do
+ActiveRecord::Schema.define(:version => 20130516091143) do
 
   create_table "apprenticeship_types", :force => true do |t|
     t.string   "apprenticeship_type"
